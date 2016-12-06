0244e1c56382d73bd88be93d31fe534fd772fd05
Nick Bennett <nick271828@gmail.com>
2016-12-06 09:36:28 -0800

    Add logging to scraping script.

--- scraper/scrape_faculty.py
+++ scraper/scrape_faculty.py
@@ -1,10 +1,12 @@
 import csv
 import json
+import logging
 import time
 from urlparse import urljoin
 from bs4 import BeautifulSoup
 import requests
 
+logging.basicConfig(level=logging.INFO)
 
 def get_faculty_netids():
     response = requests.get("http://www.kellogg.northwestern.edu/faculty/advanced_search.aspx")
@@ -39,14 +41,16 @@ def save_dict_list_to_csv(dict_list, filename):
 
 
 netids = get_faculty_netids()
+netid_count = len(netids)
+logging.info("Found {0} Faculty NetIDs.".format(netid_count))
 
 info_by_netid = {}
-print "Already have info for netids: {0}".format(', '.join(sorted(info_by_netid.keys())))
-for netid in netids:
-    if netid not in info_by_netid:
-        print "Getting info for {0}".format(netid)
+for i, netid in enumerate(netids):
+    if netid in info_by_netid:
+        logging.info("[{0} of {1}] Already have info for NetID {2}".format(i, netid_count, netid))
+    else:
+        logging.info("[{0} of {1}] Getting info for NetID {2}".format(i, netid_count, netid))
         info_by_netid[netid] = get_faculty_info_by_netid(netid)
-        time.sleep(0.2)
 
 # Add in netid to the information we've collected
 faculty = []
@@ -56,15 +60,19 @@ for netid, info in info_by_netid.iteritems():
 
 faculty = sorted(faculty, key=lambda f: f['name'])
 
+logging.info("Saving data to CSV file...")
 save_dict_list_to_csv(faculty, "faculty.csv")
 
+logging.info("Saving data to faculty.json...")
 with open('faculty.json', 'w') as f:
     json.dump(faculty, f)
 
+logging.info("Saving data (with names) to faculty_with_names.json...")
 with open('faculty_with_names.json', 'w') as f:
     faculty_with_names = filter(lambda f: f['name'], faculty)
     json.dump(faculty_with_names, f)
 
+logging.info("Saving data (with names and faces) to faculty_with_names_and_faces.json...")
 with open('faculty_with_names_and_faces.json', 'w') as f:
     faculty_with_names_and_faces = filter(lambda f: f['name'] and f['headshot_image_url'], faculty)
     json.dump(faculty_with_names_and_faces, f)
